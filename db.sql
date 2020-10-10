53f7683de58fb6987ce05957c7826f770c49a719
Desmond <desmondaldridge@gmail.com>
2020-10-09 22:49:09 -0700

    SQL ish

--- db.sql
+++ db.sql
@@ -1,27 +1,28 @@
+DROP DATABASE IF EXISTS ems_DB;
 CREATE DATABASE ems_DB;
 USE ems_DB;
 
 
 CREATE TABLE department (
   id int AUTO_INCREMENT,
-  name varchar(30) NOT NULL,
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY(id)
 );
 CREATE TABLE role (
-  id int AUTO_INCREMENT,
-  title varchar(30) NOT NULL,
-  salary decimal,
-  department_id int,
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT NOT NULL,
   PRIMARY KEY(id),
   FOREIGN KEY(department_id) REFERENCES department(department_id)
   
 );
 CREATE TABLE employee (
-  id int AUTO_INCREMENT,
-  first_name varchar(30) NOT NULL,
-  last_name DECIMAL,
-  role_id int,
-  manager_id int,
+  id INT AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30)NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
   PRIMARY KEY(id),
   FOREIGN KEY(role_id) REFERENCES role(role_id),
   FOREIGN KEY(manager_id) REFERENCES 
