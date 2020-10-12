54e4537a6b751786738776ae48e26a2ed1f8611b
Desmond <desmondaldridge@gmail.com>
2020-10-11 23:07:02 -0700

    Seed file and organizing directories

--- assets/db/db.sql
+++ assets/db/db.sql
@@ -27,8 +27,3 @@ CREATE TABLE employee (
   FOREIGN KEY(role_id) REFERENCES role(role_id),
   FOREIGN KEY(manager_id) REFERENCES 
 );
-
-INSERT INTO actors (name, coolness_points, attitude) VALUES ("Jerry", 90, "relaxed");
-INSERT INTO actors (name, coolness_points, attitude) VALUES ("Elaine", 80, "righteous");
-INSERT INTO actors (name, coolness_points, attitude) VALUES ("Kramer", 20, "doofus");
-INSERT INTO actors (name, coolness_points, attitude) VALUES ("George", 70, "selfish");
