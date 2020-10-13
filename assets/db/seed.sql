27a7a1e84627d33d88b79ca17cf5cd14d9fad9b2
Desmond <desmondaldridge@gmail.com>
2020-10-12 23:21:39 -0700

    Seed

--- assets/db/seed.sql
+++ assets/db/seed.sql
@@ -1,1 +1,36 @@
-INSERT INTO 
+INSERT INTO department (name)
+VALUES 
+("Monarch"), 
+("Small Council"), 
+("Kingsguard"), 
+("City Watch"),
+("The Faith")
+("Wardens"),
+("Night's Watch");
+
+INSERT INTO role (title, salary, department_id)
+VALUES 
+("King", 0.00, 1), 
+("Queen", 0.00, 1),
+("Hand of the King", 100000.00, 2),  
+("Master of Coin", 50000.00, 2),
+("Master of Ships", 25000.00, 2),
+("Master of Whispers", 30000.00, 2),
+("Grand Maester", 25000.00, 2),
+("Lord Commander of the Kingsguard", 25000.00, 3),
+("Kingsguard", 10000.00, 3),
+("Lord Commander of the City Watch", 15000.00, 4),
+("High Septon", 0.00, 5)
+("Warden of the East", 0.00, 6),
+("Warden of the North", 0.00, 6),
+("Warden of the South", 0.00, 6),
+("Warden of the West", 0.00, 6),
+("Lord Commander of the Night's Watch", 1000.00, 7),
+("First Ranger", 500.00, 7)
+("First Builder", 500.00, 7),
+("Lord Steward", 500.00, 7),
+("Maester of Castle Black", 500.00, 7),
+("Commander of Shadow Tower", 700.00, 7), 
+("Commander of Eastwatch-by-the-Sea", 800.00, 7),
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
